d2a78ca825fa4641d6683429f01ac85692ddbab5
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-02-10 08:02:50 +0530

    first commit

---
+++ assignmentsapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Context_API/App'
+
+
+ReactDOM.render(
+  <>
+    <App/>
+  </>,
+  document.getElementById('root')
+);
+
+
